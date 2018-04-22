dc8cad743fd6a809fb4b4f64545c7a21a3908aaf
Aaron Na <syn228@nyu.edu>
2018-04-22 01:35:31 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -10,10 +10,6 @@ def run_guessing_game
     else puts "The computer guessed #{rng}" 
     end
     
-
-    puts "Guess a number between 1 and 6."
-    rng = rand(1..6).to_s
-    user_input = gets.chomp
     end
     puts "Goodbye!"
 end
